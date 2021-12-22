cbaf15a9762cb26d97c1c7381eef9ce122582da9
Nicu Borta <nicuborta123@tutanota.com>
2021-12-22 16:45:02 +0200

    change bar alpha

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#361616";
-static const unsigned int baralpha = 0x80; // opaque is 0xff
+static const unsigned int baralpha = 0xa0; // opaque is 0xff
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
