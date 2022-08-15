54bdd2df5c561ad647e561af0050d0c4a8e30b87
Nicu <nicu@homepc.home.pc>
2022-08-15 19:22:38 +0300

    config: changes for multi-user systems

--- config.def.h
+++ config.def.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "sh", "/home/nicu/.local/bin/dmenuprogs", NULL };
+static const char *dmenucmd[] = { "sh", "/home/common/bin/dmenuprogs", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
